1ef97ee0ad73d8bf14f47c772f16a9787eda42a1
MonicaLiii <liy@union.edu>
2018-06-06 16:12:27 -0400

    Title Updated

--- sudoku_client.py
+++ sudoku_client.py
@@ -1,3 +1,11 @@
+# ------------------------------
+#       Client for sudoku.
+# ------------------------------
+# By Caleb Seymour, in Spring 2018,
+# at Union College, NY, US,
+# for the final project of CSC320 - Artificial Intelligence.
+# ------------------------------
+
 import game
 
 def main():
